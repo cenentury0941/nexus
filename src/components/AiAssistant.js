7b8be5b9a81b964e333ee78cfc599599122f3d33
Cenentury 0941 <cenentury0941@gmail.com>
2023-12-15 19:32:05 +0530

    nexus update

--- src/components/AiAssistant.js
+++ src/components/AiAssistant.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AiAssistant.css";
 
 function AiAssistant(){
+
+    const [ windowVisible , setWindowVisible ] = useState(false)
+
+    const showWindow = () => {setWindowVisible(true)}
+    
+    const hideWindow = () => {setWindowVisible(false)}
+
     return ( <div className="Assistant-Container">
-        <div className="Assistant-Show-Button"></div>
-        <div className="Assistant-Window"></div>
+        <div className="Assistant-Show-Button " onClick={showWindow}></div>
+
+        { true && <div className={"Assistant-Window " + ( windowVisible ?  "Assistant-Window-Show" : "Assistant-Window-Hide" ) }>
+            <div className="Assitant-Window-Close-Button" onClick={hideWindow}/>
+        </div>
+        }   
+
     </div> )
 }
 
