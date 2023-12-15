dc2325326ad04a6a97e0d8d3e72e63aff224d0d5
Cenentury 0941 <cenentury0941@gmail.com>
2023-12-15 19:07:24 +0530

    nexus update

--- src/components/AiAssistant.js
+++ src/components/AiAssistant.js
@@ -4,6 +4,7 @@ import "./AiAssistant.css";
 function AiAssistant(){
     return ( <div className="Assistant-Container">
         <div className="Assistant-Show-Button"></div>
+        <div className="Assistant-Window"></div>
     </div> )
 }
 
